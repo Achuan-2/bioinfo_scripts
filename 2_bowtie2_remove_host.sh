031a3330a7abefe435b7b9da5a09996a3eeda463
Achuan-2 <achuan-2@outlook.com>
2021-09-29 19:00:29 +0800

    ✨ bowtie remove host

--- 2_bowtie2_remove_host.sh
+++ 2_bowtie2_remove_host.sh
@@ -7,16 +7,22 @@
 #SBATCH -o slurm.%N.%j.%x.out        # STDOUT
 #SBATCH -e slurm.%N.%j.%x.err        # STDERR
 
-source /mnt/raid1/wuyingjian/biosoft/miniconda/etc/profile.d/conda.sh
-conda activate base
-1
-infile=$1
 
-mkdir -p 02_removed_host/${infile}/
+ID=$1
+HOST=/mnt/raid7/wangteng/buffalo/cattle/bwa_mapping/host_genomes/database/all_contamination_ref.db
+PROJECT_HOME=/mnt/raid7/Dachuang/Achuan # project folder
+DATA=${PROJECT_HOME}/01_cleandata/${ID}/${ID}
+RESULT=${PROJECT_HOME}/02_removed_host/${ID}/${ID}
 
-bowtie2 -x /mnt/raid1/wuyingjian/biosoft/database/human/hg38 -p 10 --very-sensitive -1 /mnt/raid7/wuyingjian/my_hic/internal_reference/01_cleandata/${infile}/${infile}_clean_R1.fq -2 /mnt/raid7/wuyingjian/my_hic/internal_reference/01_cleandata/${infile}/${infile}_clean_R2.fq --un-conc 02_removed_host/${infile}/${infile}_paired_clean%.fq --al-conc 02_removed_host/${infile}/${infile}_paired_contam%.fq
+mkdir -p 02_removed_host/${ID}/
 
-# bowtie2 -x /mnt/raid6/wangteng/buffalo/Representative_genome/databases/all_contamination_ref/all_contamination_ref.db -p 10 --very-sensitive -U /mnt/raid6/wangteng/buffalo/data11/analysis/01_cleandata/${infile}/${infile}_clean_unpaired_R1.fq --un 02_removed_host/${infile}/${infile}_single_clean1.fq --al 02_removed_host/${infile}/${infile}_single_contam1.fq
-# bowtie2 -x /mnt/raid6/wangteng/buffalo/Representative_genome/databases/all_contamination_ref/all_contamination_ref.db -p 10 --very-sensitive -U /mnt/raid6/wangteng/buffalo/data11/analysis/01_cleandata/${infile}/${infile}_clean_unpaired_R2.fq --un 02_removed_host/${infile}/${infile}_single_clean2.fq --al 02_removed_host/${infile}/${infile}_single_contam2.fq
+bowtie2 -x ${HOST} -p 10  --very-sensitive \
+    -1 ${DATA}_clean_R1.fq \
+    -2 ${DATA}_clean_R2.fq \
+    --un-conc ${RESULT}_paired_clean%.fq \
+    --al-conc ${RESULT}_paired_contam%.fq
+
+# bowtie2 -x /mnt/raid6/wangteng/buffalo/Representative_genome/databases/all_contamination_ref/all_contamination_ref.db -p 10 --very-sensitive -U /mnt/raid6/wangteng/buffalo/data11/analysis/01_cleandata/${ID}/${ID}_clean_unpaired_R1.fq --un 02_removed_host/${ID}/${ID}_single_clean1.fq --al 02_removed_host/${ID}/${ID}_single_contam1.fq
+# bowtie2 -x /mnt/raid6/wangteng/buffalo/Representative_genome/databases/all_contamination_ref/all_contamination_ref.db -p 10 --very-sensitive -U /mnt/raid6/wangteng/buffalo/data11/analysis/01_cleandata/${ID}/${ID}_clean_unpaired_R2.fq --un 02_removed_host/${ID}/${ID}_single_clean2.fq --al 02_removed_host/${ID}/${ID}_single_contam2.fq
 
 
