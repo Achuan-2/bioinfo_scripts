649db45d53ffb323455e17b9cedf462c939d6183
Achuan-2 <achuan-2@outlook.com>
2021-09-29 22:03:04 +0800

    ✨ megahit

---
+++ 3_assembly_megahit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+###################################################
+### ==== NUMBER OF CPUS per TASK ====
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=30G
+#SBATCH -o slurm.%x.%j.out        # STDOUT
+#SBATCH -e slurm.%x.%j.err        # STDERR
+
+source /mnt/raid1/wuyingjian/biosoft/miniconda/etc/profile.d/conda.sh
+conda activate base
+
+ID=$1
+
+echo "dealing with ${ID}"
+PROJECT_HOME=/mnt/raid7/Dachuang/Achuan
+pwd_path=`pwd`
+removed_host=${PROJECT_HOME}/02_removed_host
+contig_path=${PROJECT_HOME}/03_assembly_megahit
+RESULT=${contig_path}/${ID}/${ID}
+
+mkdir -p ${contig_path}
+
+megahit -t 10 --min-contig-len 1000 \
+        -1 ${RESULT}_paired_clean1.fq \
+        -2 ${RESULT}_paired_clean2.fq \
+        -o ${contig_path}/${ID}
+        --out-prefix ${ID}
+
+
+
