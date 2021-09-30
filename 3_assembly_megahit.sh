d6c875ef5c11d87a03992ad89429b2802651db15
Achuan-2 <achuan-2@outlook.com>
2021-09-30 22:07:58 +0800

    ✨ megahit scripts

--- 3_assembly_megahit.sh
+++ 3_assembly_megahit.sh
@@ -5,28 +5,25 @@
 #SBATCH --mem=30G
 #SBATCH -o slurm.%x.%j.out        # STDOUT
 #SBATCH -e slurm.%x.%j.err        # STDERR
+###################################################
 
-source /mnt/raid1/wuyingjian/biosoft/miniconda/etc/profile.d/conda.sh
-conda activate base
-
+# settings
 ID=$1
-
 echo "dealing with ${ID}"
 PROJECT_HOME=/mnt/raid7/Dachuang/Achuan
 pwd_path=`pwd`
 removed_host=${PROJECT_HOME}/02_removed_host
 contig_path=${PROJECT_HOME}/03_assembly_megahit
-RESULT=${contig_path}/${ID}/${ID}
+INPUT=${removed_host}/${ID}/${ID}
 
 mkdir -p ${contig_path}
 
+# run megahit
 megahit -t 10 --min-contig-len 1000 \
-        --k-list 27,37,47,57,67,77,87 \
-        --kmin-1pass -m 60e+10 
-        -1 ${RESULT}_paired_clean1.fq \
-        -2 ${RESULT}_paired_clean2.fq \
-        -o ${contig_path}/${ID}
-        --out-prefix ${ID}
-
+        --k-min 27 --k-max 127 --k-step 10 \
+        --kmin-1pass \
+        -1 ${INPUT}_paired_clean1.fq \
+        -2 ${INPUT}_paired_clean2.fq \
+        -o ${contig_path}/${ID} 
 
 
